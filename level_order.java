44c108ff6f017252f320c4f92b8ebe476a3ead5d
Darshil Shah <shahdarshil_sh@srmuniv.edu.in>
2019-04-15 10:24:28 +0530

    Added program for level order traversal

--- level_order.java
+++ level_order.java
@@ -1,4 +1,28 @@
-void LevelOrder(Node node){
+import java.util.*;
+import java.io.*;
+
+class Node {
+    Node left;
+    Node right;
+    int data;
+    
+    Node(int data) {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+}
+
+class Solution {
+
+	/* 
+    
+    class Node 
+    	int data;
+    	Node left;
+    	Node right;
+	*/
+public static void levelOrder(Node node){
     Queue<Node> q = new LinkedList<Node>();
     q.add(node);
  
@@ -14,3 +38,5 @@ void LevelOrder(Node node){
         System.out.print(n.data+" ");
     }
 }
+
+	public static Node insert(Node root, int data) {
