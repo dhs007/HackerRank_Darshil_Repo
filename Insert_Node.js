17ff67520d6e8290bd8b45a53c20421aa0410f88
Darshil Shah <shahdarshil_sh@srmuniv.edu.in>
2019-04-30 16:32:15 +0530

    Added code insert node

--- Insert_Node.js
+++ Insert_Node.js
@@ -1,138 +1,138 @@
-'use strict';
+    'use strict';
 
-const fs = require('fs');
+    const fs = require('fs');
 
-process.stdin.resume();
-process.stdin.setEncoding('utf-8');
+    process.stdin.resume();
+    process.stdin.setEncoding('utf-8');
 
-let inputString = '';
-let currentLine = 0;
+    let inputString = '';
+    let currentLine = 0;
 
-process.stdin.on('data', inputStdin => {
-    inputString += inputStdin;
-});
+    process.stdin.on('data', inputStdin => {
+        inputString += inputStdin;
+    });
 
-process.stdin.on('end', _ => {
-    inputString = inputString.replace(/\s*$/, '')
-        .split('\n')
-        .map(str => str.replace(/\s*$/, ''));
+    process.stdin.on('end', _ => {
+        inputString = inputString.replace(/\s*$/, '')
+            .split('\n')
+            .map(str => str.replace(/\s*$/, ''));
 
-    main();
-});
+        main();
+    });
 
-function readLine() {
-    return inputString[currentLine++];
-}
-
-const DoublyLinkedListNode = class {
-    constructor(nodeData) {
-        this.data = nodeData;
-        this.next = null;
-        this.prev = null;
-    }
-};
-
-const DoublyLinkedList = class {
-    constructor() {
-        this.head = null;
-        this.tail = null;
+    function readLine() {
+        return inputString[currentLine++];
     }
 
-    insertNode(nodeData) {
-        let node = new DoublyLinkedListNode(nodeData);
+    const DoublyLinkedListNode = class {
+        constructor(nodeData) {
+            this.data = nodeData;
+            this.next = null;
+            this.prev = null;
+        }
+    };
 
-        if (this.head == null) {
-            this.head = node;
-        } else {
-            this.tail.next = node;
-            node.prev = this.tail;
+    const DoublyLinkedList = class {
+        constructor() {
+            this.head = null;
+            this.tail = null;
         }
 
-        this.tail = node;
-    }
-};
+        insertNode(nodeData) {
+            let node = new DoublyLinkedListNode(nodeData);
 
-function printDoublyLinkedList(node, sep, ws) {
-    while (node != null) {
-        ws.write(String(node.data));
+            if (this.head == null) {
+                this.head = node;
+            } else {
+                this.tail.next = node;
+                node.prev = this.tail;
+            }
 
-        node = node.next;
+            this.tail = node;
+        }
+    };
 
-        if (node != null) {
-            ws.write(sep);
+    function printDoublyLinkedList(node, sep, ws) {
+        while (node != null) {
+            ws.write(String(node.data));
+
+            node = node.next;
+
+            if (node != null) {
+                ws.write(sep);
+            }
         }
     }
-}
 
-// Complete the sortedInsert function below.
+    // Complete the sortedInsert function below.
 
-/*
- * For your reference:
- *
- * DoublyLinkedListNode {
- *     int data;
- *     DoublyLinkedListNode next;
- *     DoublyLinkedListNode prev;
- * }
- *
- */
-function sortedInsert(head, data) {
-    var current = new DoublyLinkedListNode();
-    var newNode = new DoublyLinkedListNode();
-    newNode.data = data;
-    newNode.next = null;
+    /*
+     * For your reference:
+     *
+     * DoublyLinkedListNode {
+     *     int data;
+     *     DoublyLinkedListNode next;
+     *     DoublyLinkedListNode prev;
+     * }
+     *
+     */
+    function sortedInsert(head, data) {
+        var current = new DoublyLinkedListNode();
+        var newNode = new DoublyLinkedListNode();
+        newNode.data = data;
+        newNode.next = null;
 
-    if (head == null) {
-        head = newNode;
-    }
-
-    else if ((head.data) >= newNode.data) {
-        newNode.next = head;
-        newNode.next.prev = newNode; 
-        head = newNode;
-    }
-
-    else {
-        current = head;
-
-        while ((current.next != null) && (current.next.data < newNode.data)) {
-            current = current.next;
+        if (head == null) {
+            head = newNode;
         }
 
-        newNode.next = current.next;
-
-        if (current.next != null)
-            newNode.next.prev = newNode;
-
-        current.next = newNode;
-        newNode.prev = current;
-    } 
-
-    return head;
-}
-
-function main() {
-    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
-
-    const t = parseInt(readLine(), 10);
-
-    for (let tItr = 0; tItr < t; tItr++) {
-        const llistCount = parseInt(readLine(), 10);
-
-        let llist = new DoublyLinkedList();
-
-        for (let i = 0; i < llistCount; i++) {
-            const llistItem = parseInt(readLine(), 10);
-            llist.insertNode(llistItem);
+        else if ((head.data) >= newNode.data) {
+            newNode.next = head;
+            newNode.next.prev = newNode; 
+            head = newNode;
         }
 
-        const data = parseInt(readLine(), 10);
+        else {
+            current = head;
 
-        let llist1 = sortedInsert(llist.head, data);
+            while ((current.next != null) && (current.next.data < newNode.data)) {
+                current = current.next;
+            }
 
-        printDoublyLinkedList(llist1, " ", ws)
-        ws.write("\n");
+            newNode.next = current.next;
+
+            if (current.next != null)
+                newNode.next.prev = newNode;
+
+            current.next = newNode;
+            newNode.prev = current;
+        } 
+
+        return head;
     }
 
-    ws.end();
-}
+    function main() {
+        const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+        const t = parseInt(readLine(), 10);
+
+        for (let tItr = 0; tItr < t; tItr++) {
+            const llistCount = parseInt(readLine(), 10);
+
+            let llist = new DoublyLinkedList();
+
+            for (let i = 0; i < llistCount; i++) {
+                const llistItem = parseInt(readLine(), 10);
+                llist.insertNode(llistItem);
+            }
+
+            const data = parseInt(readLine(), 10);
+
+            let llist1 = sortedInsert(llist.head, data);
+
+            printDoublyLinkedList(llist1, " ", ws)
+            ws.write("\n");
+        }
+
+        ws.end();
+    }
